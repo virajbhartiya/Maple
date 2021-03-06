e9ab08fb4c752f364887024a1ba33402ca7af11d
Viraj Bhartiya <56783451+Viraj-Bhartiya@users.noreply.github.com>
2021-03-06 23:01:04 +0530

    Add files via upload

---
+++ Raspberry Pi/Time In-Out/README.md
@@ -0,0 +1,47 @@
+# Face Recognition Based Attendance System
+Makes attendance records in realtime using python's [face recognition](https://pypi.org/project/face-recognition/ "face recognition") library.
+All we need is one image of the person to be detected by the camera.
+
+_
+
+
+#### Installing on Linux
+
+First, make sure you have dlib already installed with Python bindings:
+
+  * [How to install dlib from source on macOS or Ubuntu](https://gist.github.com/ageitgey/629d75c1baac34dfa5ca2a1928a7aeaf)
+
+Cloning the repository
+```bash
+git clone https://github.com/Viraj-Bhartiya/FaceIt.git
+cd FaceIt-master
+```
+Making Virtual environment for the project
+```bash
+python3 -m venv <myvenv>
+source <myvenv>/bin/activate
+```
+Installing the required packages to our virtual environment
+```bash
+pip install -r requirements.txt
+```
+
+
+
+### Instructions
+Put all the images in 'student_db' directory & run the 'db_maker.py' file 
+```bash
+python3 db_maker.py
+```
+then excel file named 'people_db.xlsx' is made inside the 'student_db' directory
+- name of the person is extracted from the image name
+- second column has the file name (files are renamed by replacing spaces by underscores)
+- roll no. is given from 1 to N you can edit according to your needs
+
+To run the facial recognition system run 'main.py'
+```bash
+python3 main.py
+```
+###### Press q to Quit
+
+After running the program attendance record is generated with current timestamp (Ex. 'attendance_record Sun 06 Sep 2020 18:40:11 IST.xlsx' ) providing the details of the students recognised by the camera
---
+++ Raspberry Pi/Time In-Out/printing.py
@@ -0,0 +1,166 @@
+def logo():
+    print(
+        """
+    `/ooooooooo-        -ooooooooo/`         
+    m-/:::::::.          .:::..-./-m         
+    M o                          o M         
+    M -                          o M         
+    M o                          o M         
+    M -     h      //      h     o M         
+    - .     N      oo      N     . -         
+                   oo                        
+                   oo                        
+                 -oho                        
+    -                            . -         
+    M         .+`      `+.       - M         
+    M o        -+ooooooo-        / M         
+    M o                          - M         
+    M o                          o M         
+    m-/- .:::::.        .::::::::/-m         
+    `/ooooooooo-        -ooooooooo/`         
+    """
+    )
+
+
+def welcomeToFaceIt():
+    line()
+    print(
+
+        """
+
+ __          __  _                            _          __  __             _      
+ \ \        / / | |                          | |        |  \/  |           | |     
+  \ \  /\  / /__| | ___ ___  _ __ ___   ___  | |_ ___   | \  / | __ _ _ __ | | ___ 
+   \ \/  \/ / _ \ |/ __/ _ \| '_ ` _ \ / _ \ | __/ _ \  | |\/| |/ _` | '_ \| |/ _ \\
+    \  /\  /  __/ | (_| (_) | | | | | |  __/ | || (_) | | |  | | (_| | |_) | |  __/
+     \/  \/ \___|_|\___\___/|_| |_| |_|\___|  \__\___/  |_|  |_|\__,_| .__/|_|\___|
+                                                                     | |           
+                                                                     |_|           
+
+                                                                            BY- VIRAJ BHARTIYA                                                                             
+    """
+
+    )
+    line()
+
+
+def line():
+    print("""= = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = """)
+
+
+def chooseADirectory():
+    print("""
+    
+  _____  _                        _____ _                                       _____  _               _                   
+ |  __ \| |                      / ____| |                              /\     |  __ \(_)             | |                  
+ | |__) | | ___  __ _ ___  ___  | |    | |__   ___   ___  ___  ___     /  \    | |  | |_ _ __ ___  ___| |_ ___  _ __ _   _ 
+ |  ___/| |/ _ \/ _` / __|/ _ \ | |    | '_ \ / _ \ / _ \/ __|/ _ \   / /\ \   | |  | | | '__/ _ \/ __| __/ _ \| '__| | | |
+ | |    | |  __/ (_| \__ \  __/ | |____| | | | (_) | (_) \__ \  __/  / ____ \  | |__| | | | |  __/ (__| || (_) | |  | |_| |
+ |_|    |_|\___|\__,_|___/\___|  \_____|_| |_|\___/ \___/|___/\___| /_/    \_\ |_____/|_|_|  \___|\___|\__\___/|_|   \__, |
+                                                                                                                      __/ |
+                                                                                                                     |___/ 
+                                                                                                 
+        """
+
+          )
+    line()
+
+
+def creatingDatabase():
+    print(
+        """
+        
+   _____                    _    _                  _____          _          _                       
+  / ____|                  | |  (_)                |  __ \        | |        | |                      
+ | |      _ __  ___   __ _ | |_  _  _ __    __ _   | |  | |  __ _ | |_  __ _ | |__    __ _  ___   ___ 
+ | |     | '__|/ _ \ / _` || __|| || '_ \  / _` |  | |  | | / _` || __|/ _` || '_ \  / _` |/ __| / _ \\
+ | |____ | |  |  __/| (_| || |_ | || | | || (_| |  | |__| || (_| || |_| (_| || |_) || (_| |\__ \|  __/
+  \_____||_|   \___| \__,_| \__||_||_| |_| \__, |  |_____/  \__,_| \__|\__,_||_.__/  \__,_||___/ \___|
+                                            __/ |                                                    
+                                           |___/                                                     
+
+        """
+    )
+    line()
+
+
+def progressBar(iterable, prefix='', suffix='', decimals=1, length=100, fill='█', printEnd="\r"):
+    """
+    Call in a loop to create terminal progress bar
+    @params:
+        iteration   - Required  : current iteration (Int)
+        total       - Required  : total iterations (Int)
+        prefix      - Optional  : prefix string (Str)
+        suffix      - Optional  : suffix string (Str)
+        decimals    - Optional  : positive number of decimals in percent complete (Int)
+        length      - Optional  : character length of bar (Int)
+        fill        - Optional  : bar fill character (Str)
+        printEnd    - Optional  : end character (e.g. "\r", "\r\n") (Str)
+    """
+    total = len(iterable)
+    # Progress Bar Printing Function
+
+    def printProgressBar(iteration):
+        percent = ("{0:." + str(decimals) + "f}").format(100 *
+                                                         (iteration / (total)))
+        filledLength = int(length * iteration // total)
+        bar = fill * filledLength + '-' * (length - filledLength)
+        print(f'\r{prefix} |{bar}| {percent}% {suffix}', end=printEnd)
+    # Initial Call
+    printProgressBar(0)
+    # Update Progress Bar
+    for i, item in enumerate(iterable):
+        yield item
+        printProgressBar(i + 1)
+    # Print New Line on Complete
+    print()
+
+
+def productKeyInUse():
+    print(
+        """
+        
+  _____               _            _     _  __            _                        
+ |  __ \             | |          | |   | |/ /           (_)                       
+ | |__) | __ ___   __| |_   _  ___| |_  | ' / ___ _   _   _ _ __    _   _ ___  ___ 
+ |  ___/ '__/ _ \ / _` | | | |/ __| __| |  < / _ \ | | | | | '_ \  | | | / __|/ _ \\
+ | |   | | | (_) | (_| | |_| | (__| |_  | . \  __/ |_| | | | | | | | |_| \__ \  __/
+ |_|   |_|  \___/ \__,_|\__,_|\___|\__| |_|\_\___|\__, | |_|_| |_|  \__,_|___/\___|
+                                                   __/ |                           
+                                                  |___/                            
+
+        """
+    )
+
+
+def invalidKey():
+    print(
+        """
+  _  __                        _     ______                    _ 
+ | |/ /                       | |   |  ____|                  | |
+ | ' / ___ _   _   _ __   ___ | |_  | |__ ___  _   _ _ __   __| |
+ |  < / _ \ | | | | '_ \ / _ \| __| |  __/ _ \| | | | '_ \ / _` |
+ | . \  __/ |_| | | | | | (_) | |_  | | | (_) | |_| | | | | (_| |
+ |_|\_\___|\__, | |_| |_|\___/ \__| |_|  \___/ \__,_|_| |_|\__,_|
+            __/ |                                                
+           |___/                                                 
+        """
+    )
+
+
+def close():
+    print(
+        """       
+
+   _____ _           _             
+  / ____| |         (_)            
+ | |    | | ___  ___ _ _ __   __ _ 
+ | |    | |/ _ \/ __| | '_ \ / _` |
+ | |____| | (_) \__ \ | | | | (_| |
+  \_____|_|\___/|___/_|_| |_|\__, |
+                              __/ |
+                             |___/ 
+                 
+
+        """
+    )
